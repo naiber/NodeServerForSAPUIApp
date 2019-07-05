546855dae834a98c6498a86d6a184ce79a3ed977
michele <michele.spinello@spindox.it>
2019-07-05 16:06:29 +0200

    clean book route

--- routes/books.js
+++ routes/books.js
@@ -12,7 +12,6 @@ router.get('/',function(req, res,next)
 
 router.get('/:id',function(req, res,next)
 {
-    console.log('dentro get/:id')
     Book.find({_id:req.params.id},function (err ,books) {
 
         if (err) return res.status(500).json({error: err});
@@ -29,31 +28,21 @@ router.post('/',function(req,res,next)
             newBookData.save(function(err)
             {
                 if (err) return res.status(500).json({error: err});
-                res.status(201).json({message : 'data saved'});
+                res.status(201).json({message : 'Libro salvato'});
             })
         }else{
             for(var bookItem in req.body){
-                console.log('supplierItem-> ',req.body[bookItem])
                 newBookData = new Book(req.body[bookItem]);
                 newBookData.save(function(err){
                     if (err) return res.status(500).json({error: err});
                     console.log('item inserted');
                 })
             }
-            res.status(201).json({message : 'data saved'});
+            res.status(201).json({message : 'Libro salvato'});
         }
     }
 
 );
-
-// router.post('/arr',function(req,res,next)
-// {
-//   if(req.body.length && req.body.length>1){
-//   for(var i=0;i<req.body.length;i++){
-//
-//     }
-//   }
-// })
 
 router.delete('/:id',function (req, res,next)
     {
